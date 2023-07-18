bea0cf47e0f8fd7fbd3ae557153f538bd622de5a
Munim Zafar <munimzafar100@gmail.com>
2023-07-18 15:55:25 +0500

    add theme provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/toggleTheme";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
       <div>Offer Upupupup</div>
-      <Button>Click me</Button>
+      <ModeToggle />
     </main>
   );
 }
