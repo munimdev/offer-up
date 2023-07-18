05de2aed95a12b44e63a4b4c9543f26a90867d36
Munim Zafar <munimzafar100@gmail.com>
2023-07-18 15:33:24 +0500

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main className="flex flex-col items-center justify-center min-h-screen p-24">
       <div>Offer Upupupup</div>
+      <Button>Click me</Button>
     </main>
   );
 }
