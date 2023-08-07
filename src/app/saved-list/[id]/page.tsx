7686d1b59deff6b3851c827691b8222937df3e33
Munim Zafar <munimzafar100@gmail.com>
2023-08-07 22:08:55 +0500

    add pen icon

--- src/app/saved-list/[id]/page.tsx
+++ src/app/saved-list/[id]/page.tsx
@@ -4,6 +4,7 @@ import { useFetch } from "@/hooks";
 import * as Queries from "@/utils/queries";
 import Item from "@/components/saved-list/item";
 import { FavoriteList } from "@/types/types";
+import { LucidePenBox } from 'lucide-react';
 
 const ItemList = ({ params }: { params: { id: number } }) => {
   const { id } = params;
@@ -15,7 +16,12 @@ const ItemList = ({ params }: { params: { id: number } }) => {
   const item = data?.dataObject as FavoriteList;
   return (
     <div className="w-8/12 py-4 mx-auto">
-      <h1 className="mb-4 text-3xl font-bold">{item?.name}</h1>
+      <h1 className="mb-4 text-3xl font-bold inline-flex items-center gap-2">
+        {item?.name}
+        <button>
+          <LucidePenBox />
+        </button>
+      </h1>
       <div className="px-4 py-2">
         {item?.lstFavouriteListItems.map((data, i) => (
           <Item refetch={refetch} data={data} key={data.id} />
