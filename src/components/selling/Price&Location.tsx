997a13d237447d0fda14f334403402ef79294d69
Munim Zafar <munimzafar100@gmail.com>
2023-08-19 00:28:33 +0500

    fetch location

--- src/components/selling/Price&Location.tsx
+++ src/components/selling/Price&Location.tsx
@@ -1,5 +1,7 @@
 import { useAtom } from "jotai";
 import { itemFormDataAtom } from "@/utils/atoms";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
@@ -16,7 +18,6 @@ import {
 } from "@/components/ui/dialog";
 
 import MapPicker from "react-google-map-picker";
-import { useState } from "react";
 
 const DefaultLocation = { lat: 10, lng: 106 };
 const DefaultZoom = 10;
@@ -27,6 +28,10 @@ const PriceLocation = () => {
 
   const [location, setLocation] = useState(defaultLocation);
   const [zoom, setZoom] = useState(DefaultZoom);
+  const [addresses, setAddresses] = useState({
+    shortAddress: "",
+    longAddress: "",
+  });
 
   function handleChangeLocation(lat, lng) {
     setLocation({ lat: lat, lng: lng });
@@ -39,6 +44,62 @@ const PriceLocation = () => {
   function handleResetLocation() {
     setDefaultLocation({ ...DefaultLocation });
     setZoom(DefaultZoom);
+  }
+
+  async function fetchAddressFromZip(zip) {
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${zip}&key=AIzaSyAC1zTJy_NTO4dbq253Pv1VOSz_MB8YRTI`
+      );
+      const data = response.data;
+      if (data.status === "OK") {
+        const shortAddress = `${data.results[0].address_components[2].short_name}, ${data.results[0].address_components[5].short_name}`;
+        const longAddress = data.results[0].formatted_address;
+        console.log({ shortAddress, longAddress });
+        setAddresses({ shortAddress, longAddress });
+      }
+    } catch (error) {
+      console.error("Error fetching address from ZIP:", error);
+    }
+  }
+
+  async function fetchAddressFromLatLong(lat, lng) {
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyAC1zTJy_NTO4dbq253Pv1VOSz_MB8YRTI`
+      );
+      const data = response.data;
+      if (data.status === "OK") {
+        const shortAddress = `${data.results[0].address_components[2].short_name}, ${data.results[0].address_components[5].short_name}`;
+        const longAddress = data.results[0].formatted_address;
+        // You can set these to state or use them as needed
+        console.log({ shortAddress, longAddress });
+        setAddresses({ shortAddress, longAddress });
+      }
+    } catch (error) {
+      console.error("Error fetching address from lat-long:", error);
+    }
+  }
+
+  function handleZipChange() {
+    fetchAddressFromZip(itemData.zipcode);
+  }
+
+  function handleGetCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        fetchAddressFromLatLong(lat, lng);
+      },
+      (error) => {
+        console.error("Error getting current location:", error);
+      }
+    );
+  }
+
+  function handleMapLocationChange(lat, lng) {
+    fetchAddressFromLatLong(lat, lng);
   }
 
   return (
@@ -64,24 +125,38 @@ const PriceLocation = () => {
         />
       </div>
       <div className="grid w-full max-w-md gap-1.5">
-      <Button variant="outline" className="border text-primary border-primary">Get my current Location</Button>
-      <p className="text-xl text-center text-gray-200">OR</p>
-        <Label htmlFor="price">Zip Code</Label>
-        <Input
-          type="text"
-          id="zip-code"
-          placeholder="Zip Code"
-          className="font-medium border-gray placeholder:text-gray placeholder:font-medium"
-          value={itemData.zipcode}
-          onChange={(e) =>
-            setItemData({ ...itemData, zipcode: e.target.value })
-          }
-        />
+        <Button
+          variant="outline"
+          className="border text-primary border-primary"
+          onClick={handleGetCurrentLocation}
+        >
+          Get my current Location
+        </Button>
+        <p className="text-xl text-center text-gray-200">OR</p>
+        <Label htmlFor="zip-code">Zip Code</Label>
+        <div className="flex items-center gap-2">
+          <Input
+            type="text"
+            id="zip-code"
+            placeholder="Zip Code"
+            className="font-medium border-gray placeholder:text-gray placeholder:font-medium"
+            value={itemData.zipcode}
+            onChange={(e) =>
+              setItemData({ ...itemData, zipcode: e.target.value })
+            }
+          />
+          <Button onClick={handleZipChange}>Fetch Address</Button>
+        </div>
       </div>
       <p className="text-xl text-center text-gray-200">OR</p>
       <Dialog>
         <DialogTrigger asChild>
-          <Button variant="outline" className="w-full max-w-md border text-primary border-primary">Pick Location From Map</Button>
+          <Button
+            variant="outline"
+            className="w-full max-w-md border text-primary border-primary"
+          >
+            Pick Location From Map
+          </Button>
         </DialogTrigger>
         <DialogContent className="max-w-[59rem]">
           <DialogHeader>
@@ -96,7 +171,10 @@ const PriceLocation = () => {
               zoom={zoom}
               mapTypeId={"roadmap"}
               style={{ height: "600px", width: "900px" }}
-              onChangeLocation={handleChangeLocation}
+              onChangeLocation={(lat, lng) => {
+                handleChangeLocation(lat, lng);
+                handleMapLocationChange(lat, lng);
+              }}
               onChangeZoom={handleChangeZoom}
               apiKey="AIzaSyAC1zTJy_NTO4dbq253Pv1VOSz_MB8YRTI"
             />
