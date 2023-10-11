c62674285cff182cc205372dba5d08fd539db514
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2023-10-12 01:32:00 +0500

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
        <head>
         {/* Other head elements go here */}
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any"></link>
       </head>
       <body className={`${inter.className} min-h-screen flex flex-col`}>
         <QueryProvider>
