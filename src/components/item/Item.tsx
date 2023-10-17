b0eef8695f26ae208a0263df0473b17bfbfedba0
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2023-10-17 23:49:11 +0500

    item content handled

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -13,31 +13,39 @@ export const Item = ({ id, name, price,oldPrice,shortAddress, images }: TItem) =
     }
   };
   return (
-    <Link href={`/product/${id}`}>
-      <div className="flex flex-col py-2">
-        <Image
-          className="border border-gray-200 rounded-lg object-cover"
-          src={images[0]?.imagePath250 || placeholder}
-          alt=""
-          width={250}
-          height={250}
-          quality={100}
-        />
-       <span className="text-lg font-bold">{truncateString(name, 10)}</span>
-        <div className="flex flex-col justify-end h-full">
-          <div className="flex flex-row justify-between">
-            <span className="text-base font-medium">{`$${price}`}</span>
-          {oldPrice>price&&<span className="text-base font-medium line-through">{`$${oldPrice}`}</span>}  
-            {/* <span className="text-base font-medium">{additionalInfo}</span> */}
-          </div>
-          {/* <span className="text-sm font-normal text-gray-500/80">{location}</span> */}
-          <span className="text-sm font-normal text-gray-500/80">
-            {/* Islamabad, PK */}
-            {truncateString(shortAddress, 10)}
-            {/* {shortAddress} */}
-          </span>
-        </div>
+<Link href={`/product/${id}`}>
+  <div className="flex flex-col py-2">
+    <Image
+      className="border border-gray-200 rounded-lg object-cover"
+      src={images[0]?.imagePath250 || placeholder}
+      alt=""
+      width={250}
+      height={250}
+      quality={100}
+    />
+    {/* <span
+      style={{ minHeight: '3em', overflow: 'hidden' }} // Set a fixed height (adjust as needed)
+      className="text-lg font-bold"
+    >
+      {truncateString(name, 30)}
+    </span> */}
+     <span
+      style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', overflow: 'hidden', lineHeight: '1.5', minHeight: '2.9em', maxHeight:'3.0em' }}
+      className="text-lg font-bold"
+    >
+      {name}
+    </span>
+    <div className="flex flex-col justify-end h-full">
+      <div className="flex flex-row justify-between">
+        <span className="text-base font-medium">{`$${price}`}</span>
+        {oldPrice > price && <span className="text-base font-medium line-through">{`$${oldPrice}`}</span>}
       </div>
-    </Link>
+      <span className="text-sm font-normal text-gray-500/80"   style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', overflow: 'hidden', lineHeight: '1.5', minHeight: '1.5em', maxHeight:'1.7em' }}>
+      {shortAddress}
+      </span>
+    </div>
+  </div>
+</Link>
+
   );
 };
