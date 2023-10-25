3c80985bc4ea1884b033c53576d7978d7aedac39
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2023-10-26 03:34:29 +0500

    working on subCategories drop down

--- src/components/navbar/CategoriesMenu.tsx
+++ src/components/navbar/CategoriesMenu.tsx
@@ -1,63 +1,61 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react'
 import { Menu } from "lucide-react";
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuGroup,
     DropdownMenuItem,
+    DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useFetchCategories } from "@/hooks";
-
+  } from "@/components/ui/dropdown-menu";
+  import { useFetchCategories } from "@/hooks";
 const CategoriesMenu = () => {
     const { data, isLoading } = useFetchCategories();
-    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [showSubCategory,setShowSubCategory] = useState(0)
+    const handleShowSubCategories =(id:number)=>{
+        setShowSubCategory(id)
+    }
+  return (
+    <div className="block md:hidden">
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="font-bold">
+          <Menu />
+        </button>
+      </DropdownMenuTrigger>
+      {!isLoading&&<DropdownMenuContent className="w-56 max-h-[80vh] overflow-y-auto">
+      <div className="px-2 py-2 text-gray-700 font-bold">All Categories</div>
+        <DropdownMenuGroup>
+        {
+    data?.map((category) => {
+        return (
+            <>
+            <DropdownMenuItem key={category.id} 
+            onKeyUp={(event) => event.stopPropagation()}
+            onClick={event => {event.stopPropagation(); handleShowSubCategories(category.id)}} 
+            >{category.name}</DropdownMenuItem>
+            {
+    showSubCategory==category.id?category.children?.map((categoryChild) => {
+        return (
+            <>
+            <DropdownMenuItem key={categoryChild.id}>{categoryChild.name}</DropdownMenuItem>
+            </>
+        );
+    })
+:""}
+            </>
+        );
+    })
+}
 
-    const handleCategoryClick = (category) => {
-        setSelectedCategory(category);
-    };
+          
 
-    return (
-        <div className="block md:hidden">
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <button className="font-bold">
-                        <Menu />
-                    </button>
-                </DropdownMenuTrigger>
-                {!isLoading && (
-                    <DropdownMenuContent className="w-56 max-h-[80vh] overflow-y-auto">
-                        <div className="px-2 py-2 text-gray-700 font-bold">
-                            {selectedCategory ? `Subcategories of ${selectedCategory.name}` : 'All Categories'}
-                        </div>
-                        <DropdownMenuGroup>
-                            {data?.map((category) => (
-                                <DropdownMenuItem
-                                    key={category.id}
-                                    onClick={() => handleCategoryClick(category)}
-                                >
-                                    {category.name}
-                                    {selectedCategory && selectedCategory.id === category.id && (
-                                        <DropdownMenu>
-                                            <DropdownMenuContent className="w-56">
-                                                <DropdownMenuGroup>
-                                                    {category?.children?.map((childCategory) => (
-                                                        <DropdownMenuItem key={childCategory.id}>
-                                                            {childCategory.name}
-                                                        </DropdownMenuItem>
-                                                    ))}
-                                                </DropdownMenuGroup>
-                                            </DropdownMenuContent>
-                                        </DropdownMenu>
-                                    )}
-                                </DropdownMenuItem>
-                            ))}
-                        </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                )}
-            </DropdownMenu>
-        </div>
-    );
-};
 
-export default CategoriesMenu;
+        </DropdownMenuGroup>
+      </DropdownMenuContent>}
+    </DropdownMenu>
+  </div>
+  )
+}
+
+export default CategoriesMenu
