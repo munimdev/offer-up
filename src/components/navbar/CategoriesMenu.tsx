14c0be6f08725e1ec39a08d7598e536025fe9ec2
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2023-10-26 04:45:09 +0500

    Handle state of drop down

--- src/components/navbar/CategoriesMenu.tsx
+++ src/components/navbar/CategoriesMenu.tsx
@@ -5,6 +5,7 @@ import { ChevronDown, ChevronUp } from "lucide-react";
 const CategoriesMenu = () => {
   const { data, isLoading } = useFetchCategories();
   const [showSubCategory, setShowSubCategory] = useState(0);
+  const [isVisible, setIsVisible] = useState(false)
 
   const handleShowSubCategories = (id: number) => {
     setShowSubCategory(id === showSubCategory ? 0 : id);
@@ -13,10 +14,10 @@ const CategoriesMenu = () => {
   return (
     <div className="block md:hidden" >
       <div className="relative">
-        <button className="font-bold">
+        <button className="font-bold" onClick={()=>{setIsVisible(!isVisible)}}>
           <Menu />
         </button>
-        {!isLoading && (
+        {!isLoading && isVisible&&(
           <div className="absolute right-0 mt-2 w-64 max-h-[80vh] overflow-y-auto bg-white border rounded shadow-lg p-2" style={{ zIndex: 2 }}>
             <div className="px-2 py-2 text-gray-700 font-bold">
               All Categories
@@ -29,7 +30,7 @@ const CategoriesMenu = () => {
                   className="cursor-pointer px-4 py-2 hover:bg-gray-200 flex justify-between"
                   onClick={() => handleShowSubCategories(category.id)}
                 >
-                  {category.name} <ChevronDown/>
+                  {category.name} {category && category.children && category.children.length > 0 ? <ChevronDown /> : null}
                 </div>
                 {showSubCategory === category.id &&
                   category.children?.map((categoryChild) => (
