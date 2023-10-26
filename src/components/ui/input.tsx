c51a35e11a062db16b78c7d96f9accac504a3d3c
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2023-10-27 00:20:25 +0500

    handle border width in search bar

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <div
         className={cn(
-          "relative flex items-center gap-2 h-10 w-full rounded-md border border-black bg-background px-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-within:outline-none focus-within:border-blue-500 focus-within:border-2 transition-colors duration-300 ease-in-out disabled:cursor-not-allowed disabled:opacity-50",
+          "relative flex items-center gap-2 h-10 w-full rounded-md border-2 border-black bg-background px-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-within:outline-none focus-within:border-blue-500 focus-within:border-2 transition-colors duration-300 ease-in-out disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
       >
