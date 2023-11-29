cdf4f51ed18ed84a2e0f9e7d3d60155edfef439d
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2023-11-30 03:59:09 +0500

    padding issue resolved

--- src/components/selling/OptionsSelect.tsx
+++ src/components/selling/OptionsSelect.tsx
@@ -22,7 +22,7 @@ const OptionsSelect: React.FC<TOptionsSelect> = ({
 }) => {
   return (
     <Select>
-      <SelectTrigger className="w-full  p-2 sm:p-0">
+      <SelectTrigger className="w-full  p-2 ">
         <SelectValue placeholder={value&&value||title} />
       </SelectTrigger>
       <SelectContent>
