c4e8849d44c01d6b6444a6b61bc6c73e9a57e210
Abuzar Zaidi <abuzarzaidi947@gmail.com>
2023-12-16 01:24:54 +0500

    zIndex Add for navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       </head>
       <body className={`${inter.className} min-h-screen flex flex-col`}>
         <QueryProvider>
-        <nav   style={{ position: "sticky", top: 0, backgroundColor:"#ffffff" }}>
+        <nav   style={{ position: "sticky", top: 0, backgroundColor:"#ffffff",zIndex:100 }}>
         <Navbar />
     </nav>
          
